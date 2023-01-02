014f28ac32bd18c61e9c5e036cbb4d1646a6251a
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2023-01-02 07:34:57 -0800

    installed Joi package

--- index.js
+++ index.js
@@ -32,23 +32,52 @@ app.get('/', (req, res) => {
     res.send(data)
 })
 app.post('/send', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required(),
+        age: Joi.number().min(2).required(),
 
-    // const recivedData = data.push(name + age)
-    const creatData = {
+    };
+    const result = Joi.validate(req.body, schema)
+    console.log(result);
+
+    const recivedData = {
         id: data.length + 1,
         name: req.body.name,
         age: req.body.age
-
     }
-    data.push(creatData)
-    res.send(creatData)
-    console.log(creatData)
+    data.push(recivedData)
+    console.log(recivedData);
+    if (result.error) {
+        res.status(400).send({ message: result.error.details[0].message })
+    } else {
+        return res.send(recivedData)
+    }
 
 })
 
-app.put('/put', (req, res) => {
-    let id = req.params
+app.put('/:id', (req, res) => {
+    const findData = data.find(c => c.id === parseInt(req.params.id))
+    if (!findData) {
+        res.status(404).send({ message: `usernot found` })
+        res.send(findData)
+    }
 
+    const result = validateData(req.body)
+    if (result.error) {
+        res.status(400).send({ message: result.error.details[0].message })
+    }
+    findData.name(req.body.name)
+    res.send(data)
 
 })
+const validateData = () => {
+    const schema = {
+        name: Joi.string().min(3).required(),
+        age: Joi.number().min(2).required(),
+
+    };
+    const result = Joi.validate(data, schema)
+    console.log(result.error);
+}
+
 app.listen(port, console.log(`server is on ${port}`))
