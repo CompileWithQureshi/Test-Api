cc8abde871a509bc3ae175a7ee833549bf96a2e3
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2023-01-02 01:27:13 -0800

    Get and Post api is working

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 
 const port = 4000
+const data = [
+    {
+        id: 1,
+        name: "sajid",
+        age: 22
+    },
+    {
+        id: 2,
+        name: "Qureshi",
+        age: 22
+    },
+    {
+        id: 3,
+        name: "zuhaib",
+        age: 22
+    }
+
+]
 
 // const person = {
 //     name: '',
 //     age: '',
 // }
 app.get('/', (req, res) => {
-    res.send()
+    res.send(data)
 })
 app.post('/send', (req, res) => {
-    let name = req.body.name
-    let age = req.body.age
 
+    // const recivedData = data.push(name + age)
+    const creatData = {
+        id: data.length + 1,
+        name: req.body.name,
+        age: req.body.age
 
-    res.send(`${name} ${age}`)
-    console.log(`${name} ${age}`)
+    }
+    data.push(creatData)
+    res.send(creatData)
+    console.log(creatData)
+
+})
+
+app.put('/put', (req, res) => {
+    let id = req.params
+
 
 })
 app.listen(port, console.log(`server is on ${port}`))
